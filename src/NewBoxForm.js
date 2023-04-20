86937ef5fb71e49c233c11b98525c2e49c3a4920
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-20 12:11:57 -0700

    add form for create new box

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,6 +1,62 @@
-/**
- * This component should render a form that when submitted,
- * creates a new Box.
- * specify the Box’s width, height, and background color.
- * When the form is submitted, clear the input values.
+import React, { useState } from "react";
+/** Form for creating a new box to add to a list.
+ *
+ * Has state for the name/quantity of the item; on submission,
+ * sends {name, qty} to fn rec'd from parent.
+ *
+ * ShoppingList -> NewListItemForm
  */
+
+function NewBoxForm({ addBox }) {
+  const initialState = { width: "", height: "", backgroundColor: ""};
+  const [formData, setFormData] = useState(initialState);
+  /** Send {width, height, backgrounColor} to parent
+   *    & clear form. */
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    addBox(formData);
+    setFormData(initialState);
+  }
+
+  /** Update local state w/curr state of input elem */
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value,
+    }));
+  }
+
+  /** render form */
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="width">Width:</label>
+      <input
+        id="width"
+        name="width"
+        value={formData.width}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="height">Height:</label>
+      <input
+        id="height"
+        name="height"
+        value={formData.height}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="backgroundColor">Background Color:</label>
+      <input
+        id="backgroundColor"
+        name="backgroundColor"
+        value={formData.backgroundColor}
+        onChange={handleChange}
+      />
+
+      <button>Create a new box!</button>
+    </form>
+  );
+}
+
+export default NewBoxForm;
